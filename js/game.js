cae61dcee8326af9f3c3894e3ed90fdb6d16aaa4
Benjamin Muhlestein <betropper@gmail.com>
2016-08-28 16:21:33 -0400

    save

--- js/game.js
+++ js/game.js
@@ -150,7 +150,11 @@ function spawnRandom(object) {
   if (space.key.indexOf("0") != null) {
     space = getRandomSpace();
   }
-  game.add.sprite(space.selectedSpace.x*C.bg.scale,space.selectedSpace.y*C.bg.scale,object) 
+  random = game.add.sprite(space.selectedSpace.x*C.bg.scale,space.selectedSpace.y*C.bg.scale,object); 
+  random.anchor.x = .5;
+  random.anchor.y = .5;
+  random.scale.x = C.bg.scale;
+  random.scale.y = C.bg.scale;
 }
 var game = new Phaser.Game(C.game.width,C.game.height);
 game.state.add("Boot",Boot);
