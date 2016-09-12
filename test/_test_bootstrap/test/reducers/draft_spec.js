f44f8e0e8dd05c9a2619c4efa478c82b7ba47131
Benjamin Muhlestein <betropper@gmail.com>
2016-09-12 19:08:39 -0400

    save

---
+++ test/_test_bootstrap/test/reducers/draft_spec.js
@@ -0,0 +1,10 @@
+import chai from 'chai';
+const assert = chai.assert;
+
+import draft from '../../src/js/reducers/index';
+
+describe('reducers/draft', () => {
+     it('tests', () => {
+        assert.isTrue(false, 'needs tests');
+    });
+});
