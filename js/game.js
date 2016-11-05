311ba6658bf7efdc5c17548cad508fc6ee1f0253
Benjamin Muhlestein <betropper@gmail.com>
2016-11-05 12:22:43 -0400

    save

--- js/game.js
+++ js/game.js
@@ -247,7 +247,7 @@ class MainMenu {
     settingsButton.inputEnabled = true;
     var menuList = [left, countNumber, right, playerCountText, playButton, settingsButton];
 
-    var settingsText = game.add.bitmapText(game.world.centerX + game.width, game.world.centerY - game.height/7, 'attackfont', "Settings:", 90*globalScale);
+    var settingsText = game.add.bitmapText(game.world.centerX + game.width, game.world.centerY - game.height/8, 'attackfont', "Settings:", 90*globalScale);
     settingsText.anchor.set(.5);
     //Off screen settings menu
     var settingsList = [settingsText]
