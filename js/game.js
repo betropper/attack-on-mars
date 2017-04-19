13588b097051969eae939d819bf572b15f925873
Benjamin Muhlestein <betropper@gmail.com>
2017-04-19 15:04:40 -0400

    save

--- js/game.js
+++ js/game.js
@@ -9,7 +9,7 @@ localStorage.setItem('quality', globalScale);
 localStorage.setItem('qualityKey', qualitySetting);
 var C = {
  "game": {
-   "versionNumber": "1.0.1",
+   "versionNumber": "1.0.2",
    "zoomScale": 3,
    "zoomSpeed": 500,
     "moveSpeed": 900,
